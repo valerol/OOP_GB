ec01fb16b9f36e7e1d4051120f4a2eb66093f541
valerol <valerol@ya.ru>
2023-06-08 19:13:42 +1000

    init

---
+++ lesson1/src/main/java/ru/geekbrains/lesson1/sample/Program.java
@@ -0,0 +1,18 @@
+package ru.geekbrains.lesson1.sample;
+
+import ru.geekbrains.lesson1.Product;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Product product1;
+
+        product1 = new Product("ООО Лучшая вода", "Бу", -100.12);
+        //product1.brand = "ООО Лучшая вода";
+        //product1.name = "Бутылка с водой";
+        //product1.price = -50;
+        //product1.name = "A";
+       // System.out.println(product1.displayInfo());
+    }
+
+}
