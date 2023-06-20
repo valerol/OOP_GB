5e0a0c7891c9337ef995d264bba5650a70497c2c
valerol <valerol@ya.ru>
2023-06-20 14:38:31 +1000

    lesson 4 init

---
+++ lesson4/src/main/java/ru/geekbrains/lesson4/Accountable.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.lesson4;
+
+public interface Accountable<T> {
+
+    T getId();
+
+    double getAmount();
+
+    void setAmount(double amount);
+
+}
---
+++ lesson4/src/main/java/ru/geekbrains/lesson4/homework/Box.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.lesson4.homework;
+
+import java.util.ArrayList;
+
+public class Box <T extends Fruit>{
+
+    private ArrayList<T> fruits;
+
+}
