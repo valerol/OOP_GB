418b28274e3302a1864b71be2f1f7ee7588821c9
valerol <valerol@ya.ru>
2023-06-20 15:55:04 +1000

    lesson 4 task

--- lesson4/src/main/java/ru/geekbrains/lesson4/homework/Box.java
+++ lesson4/src/main/java/ru/geekbrains/lesson4/homework/Box.java
@@ -1,9 +1,28 @@
 package ru.geekbrains.lesson4.homework;
 
+import java.nio.channels.WritePendingException;
 import java.util.ArrayList;
 
 public class Box <T extends Fruit>{
 
-    private ArrayList<T> fruits;
+    private ArrayList<T> fruits = new ArrayList<T>();
+    
+    public Float getWeight() {
+        return fruits.size() > 0 ? fruits.get(0).getWeight() * fruits.size() : 0;
+    }
 
+    public void addFruit(T fruit) {
+        fruits.add(fruit);
+    }
+
+    public boolean compare(Box box) {
+        return this.getWeight().equals(box.getWeight());
+    }
+
+    public void pourTo(Box box) {
+        for(T fruit : fruits) {
+            box.addFruit(fruit);
+        }
+        fruits.removeAll(fruits);
+    }
 }
